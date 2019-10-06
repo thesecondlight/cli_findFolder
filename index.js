fbf15186b94feaf83b48e1465bf56319ace69e85
thesecondlight <1092876640@qq.com>
2019-10-07 03:20:40 +0800

    0.1.2

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // #!/user/bin/env node
 let  fs = require('fs');
 let  join = require('path').join;
-let Path='D:\\js'
-const zgit=new RegExp(/\.git$/i)
+let Path='F:\\test'
+const zgit=new RegExp(/\.git$/i) //包含.git的所有路径
+let zresult=new RegExp(/.*?(?=\\\.git)/)
 let fileNames=findSync(Path)   //存.git文件夹下的文件路径
 function findSync(startPath) { //某路径下全部文件
     let result=[];
@@ -12,16 +13,36 @@ function findSync(startPath) { //某路径下全部文件
             let fPath=join(path,val);
             let stats=fs.statSync(fPath);
             if(stats.isDirectory()) finder(fPath);
-            if(stats.isFile() && path.match(zgit)) result.push(fPath); //包含git的
+            if(stats.isFile() && path.match(zgit)){           
+                    result.push(fPath);
+            } //包含git的
         });
     }
-    finder(startPath);
-    return result;
+    finder(startPath)
+    return result
 }
 
-console.log(fileNames)
-
-//从config中取出远程地址和当前分支 所在文件夹
+//console.log(fileNames)
+//所在文件夹
+function zfilepath(){
+    let zzr=[]
+    for(let i=0;i<fileNames.length;i++){
+        const filepath=fileNames[i].match(zresult)
+        zzr.push(filepath[0])
+        //console.log("所在文件夹=>",filepath[0])  //存在重复
+    }
+    for(let i=0;i<zzr.length-1;i++){
+        if(zzr[i]=== zzr[i+1]){
+            zzr.splice(i,1)
+            i--
+        } 
+    }
+    return zzr
+}
+const zz=zfilepath()
+console.log("包含.git的文件夹",zz)
+//从config中取出远程地址和当前分支 
+let k=0
 async function doTruncate(FilePath) {
     //let filehandle = null;
     let ur=new RegExp(/(?<=url = ).*/);
@@ -30,8 +51,9 @@ async function doTruncate(FilePath) {
       const fr=await fs.readFileSync(FilePath, 'utf8');
       const url=fr.match(ur)
       const branch=fr.match(bran)
-      console.log("远程地址",url[0])
-      console.log("当前分支",branch[0])
+      
+      console.log(zz[k++],"远程地址=>",url[0])
+      console.log("当前分支=>",branch[0])
     } catch(err) {
       console.log(err)
     }
