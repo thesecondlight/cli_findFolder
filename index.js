0ff6f93e49154d0ec702008de08d5b5878e95b4f
thesecondlight <1092876640@qq.com>
2019-10-08 09:56:08 +0800

    无改动

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // #!/user/bin/env node
 let  fs = require('fs');
+let ppath=require('path')
 let  join = require('path').join;
 let Path='D:\\js'
 const zgit=new RegExp(/\.git$/i)
+const dirgit=new RegExp(/(?<=\\).*?(?=\\.git)/);
 let fileNames=findSync(Path)   //存.git文件夹下的文件路径
 function findSync(startPath) { //某路径下全部文件
     let result=[];
@@ -16,35 +18,37 @@ function findSync(startPath) { //某路径下全部文件
         });
     }
     finder(startPath);
-    return result;
+    //return result;
+    return result[0].match(dirgit)
 }
-
-console.log(fileNames)
+let pp=new RegExp(/(?<= \ ).*/);
+let result=fileNames[0].match()
+console.log(result)
 
 //从config中取出远程地址和当前分支 所在文件夹
-async function doTruncate(FilePath) {
-    //let filehandle = null;
-    let ur=new RegExp(/(?<=url = ).*/);
-    let bran=new RegExp(/(?<=branch ").*?(?="])/);
-    try {
-      const fr=await fs.readFileSync(FilePath, 'utf8');
-      const url=fr.match(ur)
-      const branch=fr.match(bran)
-      console.log("远程地址",url[0])
-      console.log("当前分支",branch[0])
-    } catch(err) {
-      console.log(err)
-    }
-  }
-let conf=new RegExp(/config$/i)
-const FilePath=[]
-for(let i=0;i<fileNames.length;i++){
-    if(fileNames[i].match(conf)){
-        FilePath.push(fileNames[i])
-        //console.log(FilePath)
-    }
-}
-for(let i=0;i<FilePath.length;i++)
-{
-    doTruncate(FilePath[i])
-}
+// async function doTruncate(FilePath) {
+//     //let filehandle = null;
+//     let ur=new RegExp(/(?<=url = ).*/);
+//     let bran=new RegExp(/(?<=branch ").*?(?="])/);
+//     try {
+//       const fr=await fs.readFileSync(FilePath, 'utf8');
+//       const url=fr.match(ur)
+//       const branch=fr.match(bran)
+//       console.log("远程地址",url[0])
+//       console.log("当前分支",branch[0])
+//     } catch(err) {
+//       console.log(err)
+//     }
+//   }
+// let conf=new RegExp(/config$/i)
+// const FilePath=[]
+// for(let i=0;i<fileNames.length;i++){
+//     if(fileNames[i].match(conf)){
+//         FilePath.push(fileNames[i])
+//         //console.log(FilePath)
+//     }
+// }
+// for(let i=0;i<FilePath.length;i++)
+// {
+//     doTruncate(FilePath[i])
+// }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,26 @@
-const FilePath=[]
+async function doTruncate(FilePath) {
+//     //let filehandle = null;
+//     let ur=new RegExp(/(?<=url = ).*/);
+//     let bran=new RegExp(/(?<=branch ").*?(?="])/);
+//     try {
+//       const fr=await fs.readFileSync(FilePath, 'utf8');
+//       const url=fr.match(ur)
+//       const branch=fr.match(bran)
+//       console.log("远程地址",url[0])
+//       console.log("当前分支",branch[0])
+//     } catch(err) {
+//       console.log(err)
+//     }
+//   }
+// let conf=new RegExp(/config$/i)
+// const FilePath=[]
 // for(let i=0;i<fileNames.length;i++){
 //     if(fileNames[i].match(conf)){
 //         FilePath.push(fileNames[i])
 //         //console.log(FilePath)
 //     }
 // }
-// for(let i=0;i<filepaths.length;i++)
+// for(let i=0;i<FilePath.length;i++)
 // {
-//     doTruncate(filepaths[i])
+//     doTruncate(FilePath[i])
 // }
